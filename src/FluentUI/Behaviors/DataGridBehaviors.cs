29bfbaed4c3ed8af665a6b8a9327bcacfc56c1c8
Francisco Torralvo <frascoj93@hotmail.com>
2023-04-02 15:27:14 +0200

    Add custom cell styles

---
+++ src/FluentUI/Behaviors/DataGridBehaviors.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FluentUI.Behaviors;
+
+public class DataGridBehaviors
+{
+    public static readonly DependencyProperty ApplyFuentStylesProperty =
+        DependencyProperty.RegisterAttached(
+            "ApplyFuentStyles",
+            typeof(bool),
+            typeof(DataGridBehaviors),
+            new PropertyMetadata(false, ApplyFuentStyles));
+
+    [Category("FluentUI")]
+    [AttachedPropertyBrowsableForType(typeof(DataGrid))]
+    public static bool GetApplyFuentStyles(UIElement element)
+    {
+        return (bool)element.GetValue(ApplyFuentStylesProperty);
+    }
+
+    [Category("FluentUI")]
+    [AttachedPropertyBrowsableForType(typeof(DataGrid))]
+    public static void SetApplyFuentStyles(UIElement element, bool value)
+    {
+        element.SetValue(ApplyFuentStylesProperty, value);
+    }
+
+    private static void ApplyFuentStyles(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.OldValue == e.NewValue)
+        {
+            return;
+        }
+
+        if (dependencyObject is not DataGrid dataGrid)
+        {
+            return;
+        }
+
+        ApplyFuentStylesChanged(dataGrid, (bool)e.NewValue);
+    }
+
+    private static void ApplyFuentStylesChanged(DataGrid dataGrid, bool newValue)
+    {
+        if (newValue)
+        {
+            Interaction.GetBehaviors(dataGrid).Add(new DataGridFluentStylesBehavior());
+        }
+        else
+        {
+            var behaviors = Interaction.GetBehaviors(dataGrid);
+            behaviors.Remove(behaviors.OfType<DataGridFluentStylesBehavior>().First());
+        }
+    }
+}
