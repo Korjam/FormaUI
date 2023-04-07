a4314f9d9e6a6ad1773f485e2d71b39847e7bde4
Francisco Torralvo <frascoj93@hotmail.com>
2023-04-07 23:55:03 +0200

    Implement tab close

--- src/FluentUI/Behaviors/TabControlBehavior.cs
+++ src/FluentUI/Behaviors/TabControlBehavior.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace FluentUI.Behaviors;
 
@@ -22,4 +23,48 @@ public static class TabControlBehavior
     [AttachedPropertyBrowsableForType(typeof(TabItem))]
     public static void SetIcon(UIElement element, object? value) =>
         element.SetValue(IconProperty, value);
+
+    public static readonly DependencyProperty CanCloseProperty =
+        DependencyProperty.RegisterAttached(
+            "CanClose",
+            typeof(bool),
+            typeof(TabControlBehavior),
+            new PropertyMetadata(false));
+
+    [Category("FluentUI")]
+    [AttachedPropertyBrowsableForType(typeof(TabItem))]
+    [AttachedPropertyBrowsableForType(typeof(TabControl))]
+    public static bool GetCanClose(UIElement element) =>
+        (bool)element.GetValue(CanCloseProperty);
+
+    [Category("FluentUI")]
+    [AttachedPropertyBrowsableForType(typeof(TabItem))]
+    [AttachedPropertyBrowsableForType(typeof(TabControl))]
+    public static void SetCanClose(UIElement element, bool value) =>
+        element.SetValue(CanCloseProperty, value);
+
+    public static readonly RoutedCommand CloseCommand = new("Close", typeof(TabControlBehavior));
+
+    static TabControlBehavior()
+    {
+        CommandManager.RegisterClassCommandBinding(
+            typeof(TabItem),
+            new(CloseCommand, OnCloseCommand, CanCloseCommand));
+    }
+
+    private static void OnCloseCommand(object sender, ExecutedRoutedEventArgs e)
+    {
+        var tabItem = (TabItem)sender;
+        ((TabControl)tabItem.Parent).Items.Remove(tabItem);
+    }
+
+    private static void CanCloseCommand(object sender, CanExecuteRoutedEventArgs e)
+    {
+        var tabItem = (TabItem)sender;
+        var tabControl = (TabControl)tabItem.Parent;
+        if (tabControl != null)
+        {
+            e.CanExecute = GetCanClose(tabItem) && GetCanClose(tabControl);
+        }
+    }
 }
