0ee371ff74c3cae80019aa67be5b93bbfd38843e
Francisco Torralvo <frascoj93@hotmail.com>
2023-04-07 23:49:43 +0200

    Support icons in tab items

---
+++ src/FluentUI/Behaviors/TabControlBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FluentUI.Behaviors;
+
+public static class TabControlBehavior
+{
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.RegisterAttached(
+            "Icon",
+            typeof(object),
+            typeof(TabControlBehavior),
+            new PropertyMetadata(null));
+
+    [Category("FluentUI")]
+    [AttachedPropertyBrowsableForType(typeof(TabItem))]
+    public static object? GetIcon(UIElement element) =>
+        (object?)element.GetValue(IconProperty);
+
+    [Category("FluentUI")]
+    [AttachedPropertyBrowsableForType(typeof(TabItem))]
+    public static void SetIcon(UIElement element, object? value) =>
+        element.SetValue(IconProperty, value);
+}
