6843a19c5515ac42f17aec146d2e2ad2393a450a
Francisco Torralvo <frascoj93@hotmail.com>
2023-04-08 00:05:22 +0200

    Update demo app

--- samples/FluentUI.Demo.ControlSample/App.xaml.cs
+++ samples/FluentUI.Demo.ControlSample/App.xaml.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace FluentUI.Demo.ControlSample
+namespace FluentUI.Demo.ControlSample;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private void Application_Startup(object sender, StartupEventArgs e)
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            AppContext.SetSwitch("Switch.System.Windows.Controls.Text.UseAdornerForTextboxSelectionRendering", false);
-        }
+        AppContext.SetSwitch("Switch.System.Windows.Controls.Text.UseAdornerForTextboxSelectionRendering", false);
     }
 }
