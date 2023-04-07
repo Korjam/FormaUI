8f85e75f2ad4dd725fada2a3bcbe96ab5e3ee22d
Francisco Torralvo <frascoj93@hotmail.com>
2023-04-07 16:29:09 +0200

    Create property for button panel content

--- src/FluentUI/Controls/FluentDialog.cs
+++ src/FluentUI/Controls/FluentDialog.cs
@@ -1,7 +1,10 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace FluentUI.Controls;
 
+[TemplatePart(Name = "PART_BottomContent", Type = typeof(ContentControl))]
 public class FluentDialog : FluentWindow
 {
     static FluentDialog()
@@ -25,4 +28,39 @@ public class FluentDialog : FluentWindow
         get => (bool)GetValue(CanCloseProperty);
         set => SetValue(CanCloseProperty, value);
     }
+
+    public static readonly DependencyProperty BottomContentProperty =
+        DependencyProperty.Register(
+            nameof(BottomContent),
+            typeof(object),
+            typeof(FluentDialog),
+            new FrameworkPropertyMetadata(null, OnBottomContentChanged));
+
+    [Bindable(true), Category("Content")]
+    public object? BottomContent
+    {
+        get => GetValue(BottomContentProperty);
+        set => SetValue(BottomContentProperty, value);
+    }
+
+    private static void OnBottomContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var ctrl = (FluentDialog)d;
+        ctrl.SetValue(HasBottomContentPropertyKey, e.NewValue != null);
+    }
+
+    private static readonly DependencyPropertyKey HasBottomContentPropertyKey =
+        DependencyProperty.RegisterReadOnly(
+            nameof(HasBottomContent),
+            typeof(bool),
+            typeof(FluentDialog),
+            new FrameworkPropertyMetadata(
+                false,
+                FrameworkPropertyMetadataOptions.None));
+
+    public static readonly DependencyProperty HasBottomContentProperty =
+            HasBottomContentPropertyKey.DependencyProperty;
+
+    [Browsable(false), ReadOnly(true)]
+    public bool HasBottomContent => (bool)GetValue(HasBottomContentProperty);
 }
