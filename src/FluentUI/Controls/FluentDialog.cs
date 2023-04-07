29f21f4c18c20a2a9a0620130f29dd1367e9cceb
Francisco Torralvo <frascoj93@hotmail.com>
2023-04-07 15:40:51 +0200

    First draft

---
+++ src/FluentUI/Controls/FluentDialog.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace FluentUI.Controls;
+
+public class FluentDialog : FluentWindow
+{
+    static FluentDialog()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(FluentDialog), new FrameworkPropertyMetadata(typeof(FluentDialog)));
+    }
+
+    public FluentDialog()
+    {
+        SetResourceReference(StyleProperty, typeof(FluentDialog));
+    }
+
+    public static readonly DependencyProperty CanCloseProperty = DependencyProperty.Register(
+        nameof(CanClose),
+        typeof(bool),
+        typeof(FluentDialog),
+        new PropertyMetadata(false));
+
+    public bool CanClose
+    {
+        get => (bool)GetValue(CanCloseProperty);
+        set => SetValue(CanCloseProperty, value);
+    }
+}
