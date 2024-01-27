d677d84a857d8f56ced04bacc9b3a079e405eab3
Francisco Torralvo <frascoj93@hotmail.com>
2024-01-27 19:01:04 +0100

    Add settings page

---
+++ samples/FormaUI.Demo.ControlSample/SettingsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace FormaUI.Demo.ControlSample;
+
+/// <summary>
+/// Interaction logic for SettingsPage.xaml
+/// </summary>
+public partial class SettingsPage : Page
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+
+    private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        // TODO: Theme change
+    }
+}
