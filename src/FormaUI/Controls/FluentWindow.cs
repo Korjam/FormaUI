50d10d1dab79e28242e954b29f50f035da5cdcdd
Francisco Torralvo <frascoj93@hotmail.com>
2024-03-21 11:28:36 +0100

    Add back button in window

--- src/FormaUI/Controls/FluentWindow.cs
+++ src/FormaUI/Controls/FluentWindow.cs
@@ -10,6 +10,32 @@ public class FluentWindow : Window
         DefaultStyleKeyProperty.OverrideMetadata(typeof(FluentWindow), new FrameworkPropertyMetadata(typeof(FluentWindow)));
     }
 
+    public static readonly DependencyProperty BackCommandProperty =
+        DependencyProperty.Register(
+            nameof(BackCommand),
+            typeof(ICommand),
+            typeof(FluentWindow),
+            new FrameworkPropertyMetadata(null));
+
+    public ICommand? BackCommand
+    {
+        get => (ICommand?)GetValue(BackCommandProperty);
+        set => SetValue(BackCommandProperty, value);
+    }
+
+    public static readonly DependencyProperty BackCommandParameterProperty =
+        DependencyProperty.Register(
+            nameof(BackCommandParameter),
+            typeof(object),
+            typeof(FluentWindow),
+            new FrameworkPropertyMetadata(null));
+
+    public object? BackCommandParameter
+    {
+        get => GetValue(BackCommandParameterProperty);
+        set => SetValue(BackCommandParameterProperty, value);
+    }
+
     public FluentWindow()
     {
         SetResourceReference(StyleProperty, typeof(FluentWindow));
